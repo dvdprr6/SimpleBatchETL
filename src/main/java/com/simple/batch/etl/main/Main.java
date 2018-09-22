b2dcec1b1fa434b3502a3b72f2960d1f019dca78
David Parr <dvdprr6@gmail.com>
2018-09-22 17:19:46 -0400

    prepare for testing

--- src/main/java/com/simple/batch/etl/main/Main.java
+++ src/main/java/com/simple/batch/etl/main/Main.java
@@ -1,9 +1,27 @@
 package com.simple.batch.etl.main;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import com.simple.batch.etl.hive.dao.EnrichedTripDao;
+import com.simple.batch.etl.hive.dao.HiveDaoFactory;
+import com.simple.batch.etl.model.EnrichedTrip;
+
 public class Main {
 	public static void main(String[] args) {
+		
+		sleep5seconds();
+		
+		List<EnrichedTrip> enrichedTripData = HiveDaoFactory.getDaoFactory(EnrichedTripDao.class, EnrichedTrip.class).getAll();
 
-
+	}
+	
+	private static void sleep5seconds() {
+		try {
+			TimeUnit.SECONDS.sleep(5);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
