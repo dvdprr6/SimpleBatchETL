b90981dd3c99ce346f3aaea902c402803c6f6215
David Parr <dvdprr6@gmail.com>
2018-09-23 13:52:02 -0400

    run stop times file dao

--- src/main/java/com/simple/batch/etl/main/Main.java
+++ src/main/java/com/simple/batch/etl/main/Main.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import com.simple.batch.etl.hbase.dao.HBaseDaoFactory;
+import com.simple.batch.etl.file.dao.FileDaoFactory;
+import com.simple.batch.etl.file.dao.StopTimesFileDao;
 import com.simple.batch.etl.hbase.dao.EnrichedTripHBaseDao;
 import com.simple.batch.etl.hive.dao.EnrichedTripHiveDao;
 import com.simple.batch.etl.hive.dao.HiveDaoFactory;
 import com.simple.batch.etl.model.EnrichedTrip;
+import com.simple.batch.etl.model.StopTimes;
 
 public class Main {
 	public static void main(String[] args) {
@@ -17,6 +20,8 @@ public class Main {
 		List<EnrichedTrip> enrichedTripData = HiveDaoFactory.getDaoFactory(EnrichedTripHiveDao.class, EnrichedTrip.class).getAll();
 
 		HBaseDaoFactory.getDaoFactory(EnrichedTripHBaseDao.class, EnrichedTrip.class).insert(enrichedTripData);
+		
+		List<StopTimes> stopTimesData = FileDaoFactory.getDaoFactory(StopTimesFileDao.class, StopTimes.class).getAll();
 	}
 	
 	private static void sleep5seconds() {
