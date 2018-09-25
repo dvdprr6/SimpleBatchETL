b224cdf0fa0a3e6058c54477df7d6702a7c8fc21
David Parr <dvdprr6@gmail.com>
2018-09-24 20:46:50 -0400

    moved file

--- sql/gtfsStm.sql
+++ sql/gtfsStm.sql
@@ -43,10 +43,37 @@ STORED AS TEXTFILE
 LOCATION '/user/vagrant/stm/gtfs/staging/frequencies'
 tblproperties('skip.header.line.count'='1');
 
+CREATE TABLE IF NOT EXISTS enriched_stop_times(
+    trip_id               STRING,
+    arrivale_time         STRING,
+    departure_time        STRING,
+    stop_id               STRING,
+    stop_sequence         STRING,
+    route_id              STRING,
+    service_id            STRING,
+    trip_headsign         STRING,
+    wheelchair_accessible STRING,
+    shape_id              STRING,
+    note_fr               STRING,
+    note_en               STRING,
+    `date`                STRING,
+    exception_type        STRING,
+    start_time            STRING,
+    end_time              STRING,
+    headway_secs          STRING
+)
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY ','
+LINES TERMINATED BY '\n'
+STORED AS TEXTFILE
+LOCATION '/user/vagrant/stm/gtfs/staging/enriched_stop_times'
+tblproperties('skip.header.line.count'='1');
+
+
 SELECT
   t.route_id, t.service_id, t.trip_id, t.trip_headsign, t.wheelchair_accessible, t.shape_id, t.note_fr, t.note_en,
-  cd.`date`, cd.exception_type,
-  f.start_time, f.end_time, f.headway_secs
+  cd.`date`, cd.exception_type, cd.service_id,
+  f.start_time, f.end_time, f.headway_secs, f.trip_id
 FROM
   ext_trips t
 JOIN
@@ -59,3 +86,4 @@ JOIN
 -- disable 'C4P:TRIP'
 -- drop 'C4P:TRIP'
 -- create 'C4P:TRIP', {NAME => 'T', VERSIONS => 5}, {NAME => 'CD', VERSIONS => 5}, {NAME => 'F', VERSIONS => 5}
+  
