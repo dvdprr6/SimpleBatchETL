ec0b0b29bd9fc5cc64c5fe02616fe1361011fe6a
David Parr <dvdprr6@gmail.com>
2018-09-24 20:49:56 -0400

    using models to represent where data belongs

--- src/main/java/com/simple/batch/etl/hive/dao/EnrichedTripHiveDao.java
+++ src/main/java/com/simple/batch/etl/hive/dao/EnrichedTripHiveDao.java
@@ -7,39 +7,56 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.simple.batch.etl.connection.HiveConnection;
-import com.simple.batch.etl.model.EnrichedTrip;
+import com.simple.batch.etl.model.CalendarDates;
+import com.simple.batch.etl.model.EnrichedTrips;
+import com.simple.batch.etl.model.Frequencies;
+import com.simple.batch.etl.model.Trips;
 import com.simple.batch.etl.utils.Constants;
 
-public class EnrichedTripHiveDao implements HiveDao<EnrichedTrip> {
+public class EnrichedTripHiveDao implements HiveDao<EnrichedTrips> {
 	
 	EnrichedTripHiveDao(){
 		
 	}
 
 	@Override
-	public List<EnrichedTrip> getAll() {
-		List<EnrichedTrip> enrichedTripData = new ArrayList<EnrichedTrip>();
+	public List<EnrichedTrips> getAll() {
+		List<EnrichedTrips> enrichedTripData = new ArrayList<EnrichedTrips>();
 		
 		try {
 			PreparedStatement preparedStatement = HiveConnection.getHiveConnection().prepareStatement(Constants.HIVE_ENRICHED_TRIP_QUERY);
 			ResultSet resultSet = preparedStatement.executeQuery();
 			
 			while(resultSet.next()) {
-				EnrichedTrip enrichedTrip = new EnrichedTrip();
-				enrichedTrip.setRouteId(resultSet.getString(Constants.HIVE_ENRICHED_TRIP_ROUTE_ID));
-				enrichedTrip.setServiceId(resultSet.getString(Constants.HIVE_ENRICHED_TRIP_SERVICE_ID));
-				enrichedTrip.setTripId(resultSet.getString(Constants.HIVE_ENRICHED_TRIP_TRIP_ID));
-				enrichedTrip.setTripHeadsign(resultSet.getString(Constants.HIVE_ENRICHED_TRIP_TRIP_HEADSIGN));
-				enrichedTrip.setWheelchairAccessible(resultSet.getString(Constants.HIVE_ENRICHED_TRIP_WHEELCHAIR_ACCESSIBLE));
-				enrichedTrip.setShapeId(resultSet.getString(Constants.HIVE_ENRICHED_TRIP_SHAPE_ID));
-				enrichedTrip.setNoteFr(resultSet.getString(Constants.HIVE_ENRICHED_TRIP_NOTE_FR));
-				enrichedTrip.setNoteEn(resultSet.getString(Constants.HIVE_ENRICHED_TRIP_NOTE_EN));
-				enrichedTrip.setDate(resultSet.getString(Constants.HIVE_ENRICHED_TRIP_DATE));
-				enrichedTrip.setExceptionType(resultSet.getString(Constants.HIVE_ENRICHED_TRIP_EXCEPTION_TYPE));
-				enrichedTrip.setStartTime(resultSet.getString(Constants.HIVE_ENRICHED_TRIP_START_TIME));
-				enrichedTrip.setEndTime(resultSet.getString(Constants.HIVE_ENRICHED_TRIP_END_TIME));
-				enrichedTrip.setHeadwaySecs(resultSet.getString(Constants.HIVE_ENRICHED_TRIP_HEADWAY_SECS));
-				enrichedTripData.add(enrichedTrip);
+				Trips trips = new Trips();
+				CalendarDates calendarDates = new CalendarDates();
+				Frequencies frequencies = new Frequencies();
+				EnrichedTrips enrichedTrips = new EnrichedTrips();
+				
+				trips.setRouteId(resultSet.getString(Constants.HIVE_ENRICHED_TRIP_ROUTE_ID));
+				trips.setServiceId(resultSet.getString(Constants.HIVE_ENRICHED_TRIP_SERVICE_ID));
+				trips.setTripId(resultSet.getString(Constants.HIVE_ENRICHED_TRIP_TRIP_ID));
+				trips.setTripHeadsign(resultSet.getString(Constants.HIVE_ENRICHED_TRIP_TRIP_HEADSIGN));
+				trips.setWheelchairAccessible(resultSet.getString(Constants.HIVE_ENRICHED_TRIP_WHEELCHAIR_ACCESSIBLE));
+				trips.setShapeId(resultSet.getString(Constants.HIVE_ENRICHED_TRIP_SHAPE_ID));
+				trips.setNoteFr(resultSet.getString(Constants.HIVE_ENRICHED_TRIP_NOTE_FR));
+				trips.setNoteEn(resultSet.getString(Constants.HIVE_ENRICHED_TRIP_NOTE_EN));
+				
+				calendarDates.setServiceId(resultSet.getString(Constants.HIVE_ENRICHED_TRIP_SERVICE_ID));
+				calendarDates.setDate(resultSet.getString(Constants.HIVE_ENRICHED_TRIP_DATE));
+				calendarDates.setExceptionType(resultSet.getString(Constants.HIVE_ENRICHED_TRIP_EXCEPTION_TYPE));
+				
+				frequencies.setTripId(resultSet.getString(Constants.HIVE_ENRICHED_TRIP_TRIP_ID));
+				frequencies.setStartTime(resultSet.getString(Constants.HIVE_ENRICHED_TRIP_START_TIME));
+				frequencies.setEndTime(resultSet.getString(Constants.HIVE_ENRICHED_TRIP_END_TIME));
+				frequencies.setHeadwaySecs(resultSet.getString(Constants.HIVE_ENRICHED_TRIP_HEADWAY_SECS));
+				
+				enrichedTrips.setTrip(trips);
+				enrichedTrips.setCalendarDates(calendarDates);
+				enrichedTrips.setFrequencies(frequencies);
+				
+				enrichedTripData.add(enrichedTrips);
+				
 			}
 			
 			resultSet.close();
